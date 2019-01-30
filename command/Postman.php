9ba13b4d7f534f54746c07e851ef4aaf899f0039
salavat <stik229@yandex.ru>
2019-01-30 04:17:15 +0400

    Проверка времени для отправки у постов

--- command/Postman.php
+++ command/Postman.php
@@ -5,16 +5,41 @@ use models\Post;
 
 class Postman
 {
+    /**
+     * Публиковать ли просроченые посты
+     * @var int
+     */
+    private $overdue = true;
+
     public function send(){
         $post = new Post();
         $pending = $post->postByPending();
 
         foreach ($pending as $index=>$value){
-            $map = [
-                'img' => json_decode($value['data'],true),
-                'message' => $value['message']
-            ];
+            $send = false;
+
+            //Самая мощьная проверка врмени публикации :)
+            $serverTime = (int)preg_replace('/[^0-9]+/ui','',date('Y-m-d H:s',time()));
+            $publishTime = (int)preg_replace('/[^0-9]+/ui','',preg_replace('/:[0-9]+$/ui','',$value['publish_time']));
+            //СРавнение придуманое в 4 часа утра
+            if($serverTime >= $publishTime){
+                if($serverTime == $publishTime){
+                    $send = true;
+                }
+                elseif ($this->overdue){
+                    //Публикация просроченых по времени постов
+                    $send = true;
+                }
+            }
+
+            if($send) {
+                $map = [
+                    'img' => json_decode($value['data'], true),
+                    'message' => $value['message'],
+                    'pubId' => $value['pubId'],
+                    'publish_time' => $value['publish_time']
+                ];
+            }
         }
-        exit(time());
     }
 }
